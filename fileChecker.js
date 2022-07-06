6176c555df898aa17557767c49fd11600c7049ad
Michelle Ward <70984572+Michelle-Ward@users.noreply.github.com>
2022-07-06 08:29:20 -0700

    add call to continously pull for information

--- fileChecker.js
+++ fileChecker.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const axios = require('axios');
+const { resourceLimits } = require('worker_threads');
 require('dotenv').config()
 
 // Gets md5 hash of file given
@@ -13,39 +14,51 @@ const getHash = (file) => {
 
 // create function to iterate over the correct output and log output
 const logResult = (result) => {
-    //check if status is 'In queue'
+    // Check if status is 'In queue'
     let scanResults = result.scan_results;
-    console.log({ fileName: result.file_info.display_name });
+    console.log(scanResults);
+    console.log(`file_name: ${result.file_info.display_name} \n`);
     if (scanResults.scan_all_result_a === 'In queue') {
-        console.log( {
-            overall_status: 'Clean'
-        })
+        console.log(`overall_status: In queue \n`);
     } else {
         let engines = Object.keys(scanResults.scan_details);
         engines.forEach( ( engine ) => {
             let details = scanResults.scan_details[engine];
-            console.log({
-                engine: engine,
-                threat_found: details.threat_found,
-                scan_result: details.scan_result_i,
-                def_time: details.def_time,
-            })
-            
+            console.log(details.threat_found)
+            console.log(`
+                engine: ${engine} \n
+                threat_found: ${details.threat_found} \n
+                scan_result: ${details.scan_result_i} \n
+                def_time: ${details.def_time}\n
+            `)
         })
     }
-    process.exit(0);
 }
+
+const handleError = (error) => {
+    //If opswat error give code
+    if (typeof error?.response?.data === 'undefined' ) {
+        console.log("Error was encountered uploading file:  ", error.code);
+        // Exit with failure
+        process.exit(1)
+    } else {
+        console.log("There was an error uploading file ", error);
+        process.exit(1)
+    }
+}
+//create a single error handler function to handle opswat related functions
+// handle non opswat errors by loggin and then exiting
 
 // Pulls path to file given with path argument
 let filePath = require('yargs/yargs')(process.argv.slice(2)).argv.path;
 
-// read file with given file path outputs a buffer
+// Read file with given file path outputs a buffer
 let file = fs.readFileSync(filePath);
 
-// get hash
+// Get hash
 let fileHash = getHash(file);
 
-
+// Lookup by hash
 axios.get(`https://api.metadefender.com/v4/hash/${fileHash}`, {
     headers: {
         "apikey": process.env.OPSWAT_KEY
@@ -53,7 +66,10 @@ axios.get(`https://api.metadefender.com/v4/hash/${fileHash}`, {
     body: "{}"
 })
     .then( (response) => {
-       logResult(response.data);
+        // File was found log out data according to expected output
+        console.log("FILE WAS FOUND");
+        logResult(response.data);
+        process.exit(0)
     })
     .catch( (error) => {
         // Check for non opswat 404 response
@@ -67,7 +83,7 @@ axios.get(`https://api.metadefender.com/v4/hash/${fileHash}`, {
         if (errorData.code === 404003) {
             // Convert buffer version of file to binary
             let binary = Buffer.from(file);
-            const options = {
+            let options = {
                 "method": "POST",
                 "headers": {
                  "apikey": process.env.OPSWAT_KEY,
@@ -75,29 +91,54 @@ axios.get(`https://api.metadefender.com/v4/hash/${fileHash}`, {
                 },
                 data: binary,
             };
+            // Upload file
             axios("https://api.metadefender.com/v4/file", options)
-                .then( ({ response }) => {
+                .then( ( response ) => {
                     console.log("GOT result ", response);
                      //get data id 
                     let dataId = response.data.data_id;
                     console.log(dataId); 
+                    let options = {
+                        "method": "GET",
+                        "url": `https://api.metadefender.com/v4/file/${dataId}`,
+                        "headers": {
+                         "apikey": process.env.OPSWAT_KEY,
+                        //  unknown what the integer should be using 1 for now
+                         "x-file-metadata": 1
+                        },
+                        "body": "{}"
+                    };
                     // TODO
+                    // keep track of file progress
+                    let progressComplete = false;
+                    //pull until progress is 100
+                    while (!progressComplete) {
+                        //make following axios call
+                        // access results
+                        // update progress value with progress off of result
+                        //result scan_results.progress_percentage
+                        //put this call into a promise and do an await
+                        axios(`https://api.metadefender.com/v4/file/${dataId}`, options)
+                            .then((result) => {
+                                console.log("result of data_id");
+                                logResult(result.data);
+                                if (result.data.scan_results.progress_percentage === 100) {
+                                    progressComplete = true;
+                                }
+                                process.exit(1)
+                            })
+                            .catch((error) => {
+                                handleError(error)
+                            })
+                    }
+
                    
                 })
                 .catch( (error) => {
-                    //If opswat error give code
-                    if (typeof error?.response?.data === 'undefined' ) {
-                        console.log("Error was encountered uploading file:  ", error.code);
-                        // Exit with failure
-                        process.exit(1)
-                    } else {
-                        console.log("There was an error uploading file ");
-                        process.exit(1)
-                    }
+                    handleError(error);
                 })
         }
     })
 
-
-
-console.log("DONE");
+    //unest and chaing the axios calls
+    // put axios for file loading into it's own function
